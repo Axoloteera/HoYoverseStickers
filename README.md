48b872bbab5a5c783c5ad5e6534d0320ef9d883b
Axoloteera <axoloteera@outlook.com>
2025-02-03 21:41:10 +0800

    Update main.py and README

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # HoYoverseStickers
-Emojis available on HoYoLAB
+A script to download stickers from HoYoverse games.
+
+## Downloading stickers now
+You can easily get stickers without running this script by the steps below:
+1. Go to [Releases](https://github.com/Axoloteera/HoYoverseStickers/releases)
+2. Download the zip file `output.zip` uploaded every month
+3. Unzip the file
+4. Enjoy!
+
+## Tips
+- All `Paimon's_Paintings` are renamed to `Paimon_s_Paintings` to avoid issues with special characters.
+- Issues and PRs are welcome.
+
+## Usage
+```bash
+python main.py
+```
+
+## Args
+- `--games`: The games to download stickers from.   
+    &emsp; e.g. `--games=genshin_impact` or `--games=genshin_impact,honkai_star_rail`.
+- `--zipped`: Whether to export the stickers as a zip file.  
+    &emsp; e.g. `--zipped` or ``.
+
+
+## License
+This script is licensed under the MIT License - see the `LICENSE` file for details.
+
+
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 import os
-
+import zipfile
 session: requests.Session = requests.Session()
 session.mount("https://", requests.adapters.HTTPAdapter(max_retries=3))
 
 game_data: dict[str, dict[str, str]] = {
     "genshin_impact": {
-        "name": "Paimon's_Paintings",
+        "name": "Paimon_s_Paintings",
         "url": r"https://genshin-impact.fandom.com/wiki/Paimon's_Paintings"
     },
     "honkai_star_rail": {
@@ -20,6 +20,7 @@ license_text = r"""
 {}:
     Community content is available under CC-BY-SA unless otherwise noted. 
 """
+
 
 def get_html(url: str) -> str:
     return session.get(url).text
@@ -36,26 +37,53 @@ def download_image(url: str, path: str) -> None:
     with open(path, "wb") as f:
         f.write(session.get(url).content)
 
+def download_game_stickers(game: str, sub_dir: str = None) -> None:
+    os.makedirs(game_data[game]["name"], exist_ok=True)
+    image_urls: list[str] = get_image_urls(get_html(game_data[game]["url"]))
+    sub_dir: str = sub_dir or game_data[game]["name"]
+    with open(f"{sub_dir}/urls.txt", "w") as f:
+        f.write("\n".join(image_urls))
+    with open(f"{sub_dir}/paths.txt", "w") as f:
+        f.write(
+            "\n".join(
+                f"{sub_dir}/{url.split('/')[-1].replace(r'%27', '_')}"
+                for url in image_urls
+            )
+        )
+    with open(f"{sub_dir}/licenses.txt", "w") as f:
+        f.write(
+            license_text.format(
+                game_data[game]["url"]
+            )
+        )
+    for url in image_urls[:1]:
+        download_image(url, f"{sub_dir}/{url.split('/')[-1].replace(r'%27', '_')}")
+
+def export_as_zipfile(sub_dirs: list[str] = None) -> None:
+    with zipfile.ZipFile("output.zip", "w") as z:
+        for sub_dir in sub_dirs:
+            for file in os.listdir(sub_dir):
+                z.write(f"{sub_dir}/{file}", f"{sub_dir}/{file.replace(r'%27', '_')}")
+
 if __name__ == "__main__":
-    for game in game_data:
-        os.makedirs(game_data[game]["name"], exist_ok=True)
-        image_urls: list[str] = get_image_urls(get_html(game_data[game]["url"]))
-        with open(f"{game_data[game]['name']}/urls.txt", "w") as f:
-            f.write("\n".join(image_urls))
-        with open(f"{game_data[game]['name']}/paths.txt", "w") as f:
-            f.write(
-                "\n".join(
-                    f"{game_data[game]['name']}/{url.split('/')[-1]}"
-                    for url in image_urls
-                )
-            )
-        with open(f"{game_data[game]['name']}/licenses.txt", "w") as f:
-            f.write(
-                license_text.format(
-                    game_data[game]["url"]
-                )
-            )
-        for url in image_urls:
-            download_image(url, f"{game_data[game]['name']}/{url.split('/')[-1]}")
+    import argparse
+
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--zipped", default=False, action="store_true", help="Save as a zip file")
+    arg_parser.add_argument("--games", 
+                            type=str, 
+                            nargs="+", 
+                            default=["genshin_impact", "honkai_star_rail"], 
+                            help="Only download specified games",
+                            choices=["genshin_impact", "honkai_star_rail"]
+                            )
+    args = arg_parser.parse_args()
+
+    for game in args.games:
+        download_game_stickers(game)
+    
+    if args.zipped:
+        export_as_zipfile([game_data[game]["name"] for game in args.games])
+
 
 
